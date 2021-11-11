28533b151d1b9b05f3a61c8ac88799f0d7c029a0
Alon Tanay <49856257+alontanay@users.noreply.github.com>
2021-11-11 09:55:10 +0200

    fix

--- linkList.js
+++ linkList.js
@@ -1,5 +1,5 @@
 let linkList = [
     {'title': 'Simple Charges Simulator', 'linkUrl': 'charges', 'thumbnail': 'charges/thumbnail.png'},
     {'title': 'The Game of CHOMP vs. bot', 'linkUrl': 'CHOMP', 'thumbnail': 'CHOMP/chomp_thumbnail.png'},
-    {'title': 'Sierpinski Triangle', 'linkUrl': 'sierpinski', 'thumbnail': 'sierpinski/thumbnail.png},
+    {'title': 'Sierpinski Triangle', 'linkUrl': 'sierpinski', 'thumbnail': 'sierpinski/thumbnail.png'},
 ];
