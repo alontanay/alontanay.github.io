e80a1f3b3c7b11732361a37627b55e25f88184a0
Alon Tanay <49856257+alontanay@users.noreply.github.com>
2022-08-30 23:44:16 +0300

    added shadow blister and created physics group

--- linkList.js
+++ linkList.js
@@ -1,9 +1,12 @@
 let linkList = {
     'General':[
-        {title: 'Simple Charges Simulator', url: 'charges', thumbnail: 'charges/thumbnail.png'},
         {title: 'The Game of CHOMP vs. bot', url: 'CHOMP', thumbnail: 'CHOMP/chomp_thumbnail.png'},
         {title: 'Sierpinski Triangle', url: 'sierpinski', thumbnail: 'sierpinski/thumbnail.png'},
     ],
+    'Physics':[
+        {title: 'Simple Charges Simulator', url: 'charges', thumbnail: 'charges/thumbnail.png'},
+        {title: 'Shadow Blister Effect', url: 'ShadowBlisterEffect', thumbnail: 'ShadowBlisterEffect/thumbnail.png'},
+    ]
     'Chemistry':[
         {title: 'Equation Balancer', url: 'chemistry/balancer'}
     ]
