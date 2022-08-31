b63578dff32b0f9e8e9931cffd51308ad141c9ca
Alon Tanay <49856257+alontanay@users.noreply.github.com>
2022-08-31 10:33:17 +0300

    fixed syntax

--- linkList.js
+++ linkList.js
@@ -1,12 +1,12 @@
 let linkList = {
     'General':[
         {title: 'The Game of CHOMP vs. bot', url: 'CHOMP', thumbnail: 'CHOMP/chomp_thumbnail.png'},
-        {title: 'Sierpinski Triangle', url: 'sierpinski', thumbnail: 'sierpinski/thumbnail.png'},
+        {title: 'Sierpinski Triangle', url: 'sierpinski', thumbnail: 'sierpinski/thumbnail.png'}
     ],
     'Physics':[
         {title: 'Simple Charges Simulator', url: 'charges', thumbnail: 'charges/thumbnail.png'},
-        {title: 'Shadow Blister Effect', url: 'ShadowBlisterEffect', thumbnail: 'ShadowBlisterEffect/thumbnail.png'},
-    ]
+        {title: 'Shadow Blister Effect', url: 'ShadowBlisterEffect', thumbnail: 'ShadowBlisterEffect/thumbnail.png'}
+    ],
     'Chemistry':[
         {title: 'Equation Balancer', url: 'chemistry/balancer'}
     ]
